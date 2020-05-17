adaddd48d3735eeb1ec46604013bf3e9d8e0dd83
Nick Hart <nhartmusic@gmail.com>
2020-05-17 15:30:06 +0200

    authenticated users can now  update the tracks they created

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -27,9 +27,34 @@ class CreateTrack(graphene.Mutation):
         if user.is_anonymous:
             raise Exception('You must login to add tracks, you fucking idiot')
 
-        track = Track(title=title, description=description, url=url, postedBy=user)
+        track = Track(title=title, description=description, url=url, posted_by=user)
         track.save()
         return CreateTrack(track=track)
 
+class UpdateTrack(graphene.Mutation):
+    track = graphene.Field(TrackType)
+
+    class Arguments:
+        track_id = graphene.Int(required=True)
+        title = graphene.String()
+        description = graphene.String()
+        url = graphene.String()
+
+    def mutate(self, info, track_id, title, url, description):
+        user = info.context.user
+        track = Track.objects.get(id=track_id)
+
+        if track.posted_by != user:
+            raise Exception('This user cannot update this track because this user SUCKS')
+
+        track.title = title
+        track.description = description
+        track.url = url
+
+        track.save()
+
+        return UpdateTrack(track=track)
+
 class Mutation(graphene.ObjectType):
     create_track = CreateTrack.Field()
+    update_track = UpdateTrack.Field()
