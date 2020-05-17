abfbc4f67622c2b33a1e992b057c6bfc8fe7b464
Nick Hart <nhartmusic@gmail.com>
2020-05-17 14:22:58 +0200

    fixed naming convention with migration

--- app/tracks/models.py
+++ app/tracks/models.py
@@ -7,5 +7,5 @@ class Track(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     url = models.URLField()
-    createdAt = models.DateTimeField(auto_now_add=True)
-    postedBy = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    posted_by = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE)
