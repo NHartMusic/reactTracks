eb9164494fad59b4e027835aa782b0aef0e2ebb5
Nick Hart <nhartmusic@gmail.com>
2020-05-19 16:11:00 +0200

    cors access working, connecting django backend to react frontend

--- react-tracks-client/src/Root.js
+++ react-tracks-client/src/Root.js
@@ -1,6 +1,30 @@
 import React from "react";
 import withRoot from "./withRoot";
 
-const Root = () => <div>Root</div>;
+//apollo imports
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
+
+const Root = () => (
+    <Query query={GET_TRACKS_QUERY}>
+        {({ data, loading, error }) => {
+            if (loading) return <div>This thang is loading</div>
+            if (error) return <div>Something got fucked up</div>
+
+            return <div>{JSON.stringify(data)}</div>
+        }}
+    </Query>
+)
+
+const GET_TRACKS_QUERY = gql`
+    {
+        tracks {
+            id
+            title
+            description
+            url
+        }
+    }
+`
 
 export default withRoot(Root);
