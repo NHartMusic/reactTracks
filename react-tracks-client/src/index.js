b909d094d5a91ab1fe23063b8e6f2aa8a5d91c55
Nick Hart <nhartmusic@gmail.com>
2020-05-24 15:55:42 +0200

    using apollo client state to manage auth state, i think its working?

--- react-tracks-client/src/index.js
+++ react-tracks-client/src/index.js
@@ -8,15 +8,29 @@ import Root from './Root'
 import Auth from './components/Auth'
 
 //apollo imports
-import { ApolloProvider } from 'react-apollo'
-import ApolloClient from 'apollo-boost'
+import { ApolloProvider, Query } from 'react-apollo'
+import ApolloClient, { gql } from 'apollo-boost'
 
 const client = new ApolloClient({
-    uri: 'http://localhost:8000/graphql/'
+    uri: 'http://localhost:8000/graphql/',
+    clientState: {
+        defaults: {
+            isLoggedIn: !!localStorage.getItem('authToken')
+        }
+    }
 })
+
+const IS_LOGGED_IN_QUERY = gql`
+    query {
+        isLoggedIn @client
+    }
+`
 
 ReactDOM.render(
     <ApolloProvider client={client}>
+        <Query query={IS_LOGGED_IN_QUERY}>
+            {({ data }) => data.isLoggedIn ? <Root /> : <Auth />}
+        </Query>
         <Auth />
     </ApolloProvider>,
     document.getElementById('root'))
